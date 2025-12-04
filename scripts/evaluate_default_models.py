3393b6b6637b55d728c16d853cc80abc89d816a5
Shrabanti Bala Joya <shrabantibala1502@gmail.com>
2025-12-04 15:48:12 -0800

    Removed models and utils func

--- scripts/evaluate_default_models.py
+++ scripts/evaluate_default_models.py
@@ -1,72 +1,33 @@
 import click
-import numpy as np
 import pandas as pd
 from pathlib import Path
 import pickle
 
-from sklearn.metrics import make_scorer, fbeta_score
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_validate
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
-from sklearn.compose import make_column_transformer
+from sklearn.metrics import make_scorer, fbeta_score
+from utils import mean_std_cross_val_scores
+from models import get_models
 
-from sklearn.dummy import DummyClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-
-# this one goes to utils.py
-def mean_std_cross_val_scores(model, X_train, y_train, **kwargs):
-    """
-    Returns mean and std of cross validation
-
-    Parameters
-    ----------
-    model :
-        scikit-learn model
-    X_train : numpy array or pandas DataFrame
-        X in the training data
-    y_train :
-        y in the training data
-
-    Returns
-    ----------
-        pandas Series with mean scores from cross_validation
-    """
-    scores = cross_validate(model, X_train, y_train, **kwargs)
-    mean_scores = pd.DataFrame(scores).mean()
-    std_scores = pd.DataFrame(scores).std()
-    out_col = []
-    for i in range(len(mean_scores)):
-        out_col.append((f"%0.3f (+/- %0.3f)" % (mean_scores.iloc[i], std_scores.iloc[i])))
-    return pd.Series(data=out_col, index=mean_scores.index)
     
 @click.command()
-@click.option('--X_train', required=True, help='Path to X_train data CSV')
-@click.option('--y_train', required=True, help='Path to y_train data CSV')
-@click.option('--preprocessor', required=True, help='Path to preprocessor')
+@click.option('--X_train_path', required=True, help='Path to X_train data CSV')
+@click.option('--y_train_path', required=True, help='Path to y_train data CSV')
+@click.option('--preprocessor_path', required=True, help='Path to preprocessor')
+@click.option('--pos_label', default='Heart Disease', help='Positive class label for fbeta_score')
 @click.option('--beta', default=2.0, help='Beta parameter for fbeta_score')
 @click.option('--random-state', default=123, help='Random state for classifiers')
-@click.option('--results', required=True, help='Path to save results table')
+@click.option('--results', required=True, help='File path to save results table, include name of the CSV file e.g., results/CV_scores_default_parameters.csv')
 
-def main(X_train, y_train, preprocessor, beta, random_state, results):
-    Path(results).mkdir(parents=True, exist_ok=True)
+def main(X_train_path, y_train_path, preprocessor_path, pos_label, beta, random_state, results):
 
-    X_train = pd.read_csv(X_train)
-    y_train = pd.read_csv(y_train).squeeze("columns")
+    X_train = pd.read_csv(X_train_path)
+    y_train = pd.read_csv(y_train_path).squeeze("columns")
 
-    with open(preprocessor, "rb") as f:
+    with open(preprocessor_path, "rb") as f:
         preprocessor = pickle.load(f)
 
-    models = {
-        "dummy clf": DummyClassifier(strategy='most_frequent'),
-        "decision tree": DecisionTreeClassifier(random_state=random_state),
-        "Logistic Regression": LogisticRegression(random_state=random_state),
-        "RBF SVM": SVC(random_state=random_state)
-    }
-
-    scorer = make_scorer(fbeta_score, pos_label='Heart Disease', beta=beta)
+    models = get_models(random_state=random_state)
+    scorer = make_scorer(fbeta_score, pos_label=pos_label, beta=beta)
 
     results_dict = {}
     for name, model in models.items():
@@ -75,8 +36,11 @@ def main(X_train, y_train, preprocessor, beta, random_state, results):
             pipe, X_train, y_train, cv=5, return_train_score=True, scoring=scorer
         )
     results_df = pd.DataFrame(results_dict).T
+
+    results_path = Path(results)
+    results_path.parent.mkdir(parents=True, exist_ok=True)
     
-    results_df.to_csv(Path(results) / "CV_scores_default_parameters.csv", index=True)
+    results_df.to_csv(results_path, index=True)
 
 
 if __name__ == "__main__":
