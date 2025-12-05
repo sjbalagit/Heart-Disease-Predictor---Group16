a95d02bd0b06df6369d073deb5d23c31148fc95e
sjbalagit <shrabantibala1502@gmail.com>
2025-12-05 09:46:07 -0800

    Fix parameter name from 'random-state' to 'random_state'

--- scripts/evaluate_default_models.py
+++ scripts/evaluate_default_models.py
@@ -15,7 +15,7 @@ from models import get_models
 @click.option('--preprocessor_path', required=True, help='Path to preprocessor')
 @click.option('--pos_label', default='Heart Disease', help='Positive class label for fbeta_score')
 @click.option('--beta', default=2.0, help='Beta parameter for fbeta_score')
-@click.option('--random-state', default=123, help='Random state for classifiers')
+@click.option('--random_state', default=123, help='Random state for classifiers')
 @click.option('--results', required=True, help='File path to save results table, include name of the CSV file e.g., results/CV_scores_default_parameters.csv')
 
 def main(X_train_path, y_train_path, preprocessor_path, pos_label, beta, random_state, results):
